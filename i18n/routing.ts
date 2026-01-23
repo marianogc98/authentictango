265edf94acc1e47eb9c3f193710bc3b33c6f45bd
Marianogc98 <marianogc98@hotmail.com>
2026-01-23 12:49:44 -0300

    Cambiar locale por defecto a inglÃ©s y replicar contenido de inglÃ©s a espaÃ±ol

--- i18n/routing.ts
+++ i18n/routing.ts
@@ -2,6 +2,6 @@ import { defineRouting } from 'next-intl/routing';
 
 export const routing = defineRouting({
   locales: ['en', 'es'],
-  defaultLocale: 'es',
+  defaultLocale: 'en',
   localePrefix: 'always'
 });
